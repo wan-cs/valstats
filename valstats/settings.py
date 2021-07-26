33bf462013a0321c39a3b0259c2eaa0c4a2d1f86
wan-cs <jdw159@case.edu>
2021-07-26 16:53:10 -0500

    setup basic user login/logout

--- valstats/settings.py
+++ valstats/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'valstats-home'
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
